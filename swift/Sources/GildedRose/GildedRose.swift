63d94752f3f8bbab4713a642be8aea41021380c0
Maxence Leroy <maxencel@bam.tech>
2021-09-15 14:54:52 +0200

    refactor: add min max quality constants

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -5,27 +5,30 @@ public class GildedRose {
         self.items = items
     }
     
+    private let minimumQuality = 0
+    private let maximumQuality = 50
+    
     public func updateQuality() {
         for i in 0 ..< items.count {
             if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
+                if (items[i].quality > minimumQuality) {
                     if (items[i].name != "Sulfuras, Hand of Ragnaros") {
                         items[i].quality = items[i].quality - 1
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
+                if (items[i].quality < maximumQuality) {
                     items[i].quality = items[i].quality + 1
                     
                     if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
                         if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
+                            if (items[i].quality < maximumQuality) {
                                 items[i].quality = items[i].quality + 1
                             }
                         }
                         
                         if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
+                            if (items[i].quality < maximumQuality) {
                                 items[i].quality = items[i].quality + 1
                             }
                         }
@@ -40,7 +43,7 @@ public class GildedRose {
             if (items[i].sellIn < 0) {
                 if (items[i].name != "Aged Brie") {
                     if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
+                        if (items[i].quality > minimumQuality) {
                             if (items[i].name != "Sulfuras, Hand of Ragnaros") {
                                 items[i].quality = items[i].quality - 1
                             }
@@ -49,7 +52,7 @@ public class GildedRose {
                         items[i].quality = items[i].quality - items[i].quality
                     }
                 } else {
-                    if (items[i].quality < 50) {
+                    if (items[i].quality < maximumQuality) {
                         items[i].quality = items[i].quality + 1
                     }
                 }
