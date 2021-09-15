5031928abe1f1aeb1ce80a9cbb006039ab5213d2
Maxence Leroy <maxencel@bam.tech>
2021-09-15 15:03:12 +0200

    refactor: extract items[i] into variable

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -9,51 +9,51 @@ public class GildedRose {
     private let maximumQuality = 50
     
     public func updateQuality() {
-        for i in 0 ..< items.count {
-            if (items[i].itemType() != .cheese && items[i].itemType() != .backstagePass) {
-                if (items[i].quality > minimumQuality) {
-                    if (items[i].itemType() != .legendary) {
-                        items[i].quality = items[i].quality - 1
+        for item in items {
+            if (item.itemType() != .cheese && item.itemType() != .backstagePass) {
+                if (item.quality > minimumQuality) {
+                    if (item.itemType() != .legendary) {
+                        item.quality = item.quality - 1
                     }
                 }
             } else {
-                if (items[i].quality < maximumQuality) {
-                    items[i].quality = items[i].quality + 1
+                if (item.quality < maximumQuality) {
+                    item.quality = item.quality + 1
                     
-                    if (items[i].itemType() == .backstagePass) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < maximumQuality) {
-                                items[i].quality = items[i].quality + 1
+                    if (item.itemType() == .backstagePass) {
+                        if (item.sellIn < 11) {
+                            if (item.quality < maximumQuality) {
+                                item.quality = item.quality + 1
                             }
                         }
                         
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < maximumQuality) {
-                                items[i].quality = items[i].quality + 1
+                        if (item.sellIn < 6) {
+                            if (item.quality < maximumQuality) {
+                                item.quality = item.quality + 1
                             }
                         }
                     }
                 }
             }
             
-            if (items[i].itemType() != .legendary) {
-                items[i].sellIn = items[i].sellIn - 1
+            if (item.itemType() != .legendary) {
+                item.sellIn = item.sellIn - 1
             }
             
-            if (items[i].sellIn < 0) {
-                if (items[i].itemType() != .cheese) {
-                    if (items[i].itemType() != .backstagePass) {
-                        if (items[i].quality > minimumQuality) {
-                            if (items[i].itemType() != .legendary) {
-                                items[i].quality = items[i].quality - 1
+            if (item.sellIn < 0) {
+                if (item.itemType() != .cheese) {
+                    if (item.itemType() != .backstagePass) {
+                        if (item.quality > minimumQuality) {
+                            if (item.itemType() != .legendary) {
+                                item.quality = item.quality - 1
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality
+                        item.quality = item.quality - item.quality
                     }
                 } else {
-                    if (items[i].quality < maximumQuality) {
-                        items[i].quality = items[i].quality + 1
+                    if (item.quality < maximumQuality) {
+                        item.quality = item.quality + 1
                     }
                 }
             }
