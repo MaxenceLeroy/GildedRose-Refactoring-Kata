f6a474809cd8481e4aa30bc89129253f9b1724aa
Maxence Leroy <maxencel@bam.tech>
2021-09-15 15:17:17 +0200

    refactor: divide update quality in 3 functions

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -16,42 +16,53 @@ public class GildedRose {
         item.quality = max(item.quality - amount, minimumQuality)
     }
     
-    public func updateQuality() {
-        for item in items {
-            if (item.itemType() != .cheese && item.itemType() != .backstagePass) {
+    private func updateItemQuality(item: Item) {
+        if (item.itemType() != .cheese && item.itemType() != .backstagePass) {
+            if (item.itemType() != .legendary) {
+                decreaseItemQuality(item: item, amount: 1)
+            }
+        } else {
+            increaseItemQuality(item: item, amount: 1)
+            
+            if (item.itemType() == .backstagePass) {
+                if (item.sellIn < 11) {
+                    increaseItemQuality(item: item, amount: 1)
+                }
+                
+                if (item.sellIn < 6) {
+                    increaseItemQuality(item: item, amount: 1)
+                }
+            }
+        }
+    }
+    
+    private func updateItemSellIn(item: Item) {
+        if (item.itemType() != .legendary) {
+            item.sellIn = item.sellIn - 1
+        }
+    }
+    
+    private func updateExpiredItemQuality(item: Item) {
+        if (item.itemType() != .cheese) {
+            if (item.itemType() != .backstagePass) {
                 if (item.itemType() != .legendary) {
                     decreaseItemQuality(item: item, amount: 1)
                 }
             } else {
-                increaseItemQuality(item: item, amount: 1)
-                
-                if (item.itemType() == .backstagePass) {
-                    if (item.sellIn < 11) {
-                        increaseItemQuality(item: item, amount: 1)
-                    }
-                    
-                    if (item.sellIn < 6) {
-                        increaseItemQuality(item: item, amount: 1)
-                    }
-                }
+                decreaseItemQuality(item: item, amount: item.quality)
             }
+        } else {
+            increaseItemQuality(item: item, amount: 1)
+        }
+    }
+    
+    public func updateQuality() {
+        for item in items {
+            updateItemQuality(item: item)
+            updateItemSellIn(item: item)
             
-            if (item.itemType() != .legendary) {
-                item.sellIn = item.sellIn - 1
-            }
-            
-            if (item.sellIn < 0) {
-                if (item.itemType() != .cheese) {
-                    if (item.itemType() != .backstagePass) {
-                        if (item.itemType() != .legendary) {
-                            decreaseItemQuality(item: item, amount: 1)
-                        }
-                    } else {
-                        decreaseItemQuality(item: item, amount: item.quality)
-                    }
-                } else {
-                    increaseItemQuality(item: item, amount: 1)
-                }
+            if(item.sellIn < 0) {
+                updateExpiredItemQuality(item: item)
             }
         }
     }
