3ba2cd5b3b563b3e87a9da5a8d97aa48a70f0d52
Maxence Leroy <maxencel@bam.tech>
2021-09-15 14:24:46 +0200

    test: add sell in tests

--- swift/Tests/GildedRoseTests/GildedRoseTests.swift
+++ swift/Tests/GildedRoseTests/GildedRoseTests.swift
@@ -10,7 +10,31 @@ class GildedRoseTests: XCTestCase {
         XCTAssertEqual(app.items[0].name, "foo")
     }
 
+    func testDecreaseSellIn() {
+        let items = [Item(name: "foo", sellIn: 2, quality: 0)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[0].sellIn, 1)
+    }
+
+    func testNotDecreaseSellInLegedaryItem() {
+        let items = [Item(name: "Sulfuras, Hand of Ragnaros", sellIn: 2, quality: 80)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[0].sellIn, 2)
+    }
+
+    func testSellInCanGoNegative() {
+        let items = [Item(name: "foo", sellIn: 0, quality: 0)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[0].sellIn, -1)
+    }
+
     static var allTests = [
         ("testFoo", testFoo),
+        ("testDecreaseSellIn", testDecreaseSellIn),
+        ("testNotDecreaseSellInLegedaryItem", testNotDecreaseSellInLegedaryItem),
+        ("testSellInCanGoNegative", testSellInCanGoNegative)
     ]
 }
